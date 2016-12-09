c9b3db3b225edf7fce3ca79dcbf70d474a8ad3c8
cmhnk <corinne@automatedinsights.com>
2016-12-09 13:48:07 -0500

    get leaderboard working

--- guessing_game.rb
+++ guessing_game.rb
@@ -2,10 +2,25 @@ def user_won?(current_guess, computer)
   current_guess == computer
 end
 
-def winner_display
-  puts "Oh mah gad, that's the number I chose! You WIN!"
-  puts 'Here is the leaderboard:'
-  puts 'Example.........Score'
+def winner_display(name, total_guesses)
+  puts 'Oh mah gad, that\'s the number I chose! You WIN!'
+  puts """
+  Adding you to the leaderboard...
+  """
+end
+
+# array of hashes
+def leaderboard(current, computer, name, total_guesses, winners)
+  if user_won?(current, computer)
+    timestamp = Time.now
+    winners << {:name => name, :score => (total_guesses.length + 1), :time => timestamp.inspect}
+  end
+  puts "~~~~~~~~HIGH SCORES:~~~~~~~~\n\n"
+  my_winners = winners.sort_by {|winner| winner[:score]}
+
+  my_winners.each do |winner|
+    puts "#{winner[:name]}\t\t\t #{winner[:score]}"
+  end
 end
 
 
@@ -14,12 +29,12 @@ end
 #  puts 'Here is the leaderboard:'
 #end
 
-def again?
+def again?(winners)
   puts 'Would you like to play again? Enter y or n'
   puts '>'
   answer = gets.chomp
   if answer == 'y'
-    main
+    main(winners)
   elsif answer == 'n'
     'Okay. Exiting game. Goodbye!'
     exit
@@ -44,7 +59,8 @@ def compare(current, computer)
   end
 end
 
-def main
+
+def main(winners)
   puts "Enter your name:"
   name = gets.chomp
 
@@ -67,8 +83,9 @@ def main
     current = gets.chomp.to_i
     feedback << compare(current, computer)
     if user_won?(current, computer)
-      winner_display
-      again?
+      winner_display(name, total_guesses)
+      leaderboard(current, computer, name, total_guesses, winners)
+      again?(winners)
     end
     informative_guesses << current
     total_guesses << current
@@ -78,8 +95,9 @@ def main
   while informative_guesses.length < 2 do
     current = gets.chomp.to_i
     if user_won?(current, computer)
-      winner_display
-      again?
+      winner_display(name, total_guesses)
+      leaderboard(current, computer, name, total_guesses, winners)
+      again?(winners)
     elsif feedback[-1] == 'H'
       if current < informative_guesses[-1]
         feedback << compare(current, computer)
@@ -104,8 +122,9 @@ def main
     current = gets.chomp.to_i
     info = available_info_hash[feedback[-2]][feedback[-1]]
     if user_won?(current, computer)
-      winner_display
-      again?
+      winner_display(name, total_guesses)
+      leaderboard(current, computer, name, total_guesses, winners)
+      again?(winners)
     elsif info == 'min'
       # guess should be greater than lower boundary, which is guesses[-1]
       if current > informative_guesses[-1] #normal mode
@@ -146,10 +165,15 @@ def main
 
   if user_won?(current, computer)==FALSE && trials == 11
     puts "\nSorry, the number was #{computer}. You lose. Better luck next time!"
-    again?
+    again?(winners)
   end
 end
 
+winners =[]
+main(winners)
 
-
-main
+# current bugs to address:
+# trials does not end at 11
+# losing doesn't appear to happen
+# when 2 high scorers have the same score, defaults to order them by alphabet
+# add time instead 
